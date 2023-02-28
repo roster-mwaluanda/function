20c6219a7f078bf98edecfb86eb3702398e25d07
roster-mwaluanda <mwaluandaroster@gmail.com>
2023-02-28 02:57:56 -0800

    my function assignment

---
+++ function.dart
@@ -0,0 +1,47 @@
+void main(){
+addTwo(2, 4);
+subtractTwo(10,15);
+multiplyTwo(2.4, 5);
+divideTwo(24, 4);
+
+String gfg="welcome to GreekforGreeks";
+stringLength(gfg);
+
+}
+void addTwo(int n1,int n2){
+  int n3=n1+n2;
+  print("sum of two numbers is $n3\n");
+}
+void subtractTwo(var a,var b){
+int c=a-b;
+print("the difference between teo numbers are $c\n");
+
+}
+void multiplyTwo(double a,double b){
+double c= a*b;
+print("the multiplication of two numbers are $c\n");
+
+}
+void divideTwo(double x,double y){
+   double z= x/y;
+   print("the quotied of two numbers are $z");
+}
+void stringLength(String gfg ){// Main function
+
+	
+// Declaring a string variable
+String gfg = "Welcome to dart programming language";
+	
+// Finding the length
+int length = gfg.length;
+	
+// Printing the length of the string
+print(length);
+
+}
+void getFirstElement(){
+
+
+  
+}
+
